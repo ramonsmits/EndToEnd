ff23f8aba591502f81f9be4ff1a3a340ef256871
Ramon Smits <ramon.smits@gmail.com>
2016-04-01 14:38:58 +0200

    Added variables with values based on variables.md

---
+++ src/PerformanceTests/Tests/Variables/NServiceBusVersion.cs
@@ -0,0 +1,80 @@
+﻿enum NServiceBusVersion
+{
+    V5,
+    V6
+}
+
+enum IOPS
+{
+    Slow, // HDD
+    Normal, // SSD
+    Fast // SSD M2
+}
+
+
+enum Platform
+{
+    x86,
+    x64
+}
+
+enum gcServer
+{
+    On,
+    Off
+}
+
+enum Transport
+{
+    MSMQ,
+    RMQ,
+    ASQ,
+    ASB,
+}
+
+enum Persistence
+{
+    InMemory,
+    NHibernate,
+    RavenDB,
+    MSMQ
+}
+
+enum Serialization
+{
+    Json,
+    Xml
+}
+
+enum MessageSize
+{
+    Tiny,
+    Small,
+    Medium,
+    Large
+}
+
+enum Outbox
+{
+    On,
+    Off
+}
+
+enum DTC
+{
+    On,
+    Off
+}
+
+enum TransactionMode
+{
+    Receive,
+    Unreliable,
+    AtomicSends
+}
+
+enum Audit
+{
+    On,
+    Off
+}
