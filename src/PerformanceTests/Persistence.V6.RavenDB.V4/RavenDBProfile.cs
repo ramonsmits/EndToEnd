a3da7c22afdf118a11c96b66cda65ea40ed54c5b
Ramon Smits <ramon.smits@gmail.com>
2016-04-04 19:46:48 +0200

    Added profiles for persistence options InMemory, RavenDB and NHibernate.

---
+++ src/PerformanceTests/Persistence.V6.RavenDB.V4/RavenDBProfile.cs
@@ -0,0 +1,9 @@
+﻿using NServiceBus;
+
+class RavenDBProfile : IProfile
+{
+    public void Configure(EndpointConfiguration endpointConfiguration)
+    {
+        endpointConfiguration.UsePersistence<RavenDBPersistence>();
+    }
+}
