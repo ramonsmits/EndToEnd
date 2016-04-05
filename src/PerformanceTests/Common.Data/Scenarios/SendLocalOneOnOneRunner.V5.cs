1698233f9e58dde4fe1286c57adce89c5a0be250
Ramon Smits <ramon.smits@gmail.com>
2016-04-05 15:56:58 +0200

    Added scenario SendLocalOneOnOne.

---
+++ src/PerformanceTests/Common.Data/Scenarios/SendLocalOneOnOneRunner.V5.cs
@@ -0,0 +1,26 @@
+﻿#if Version5
+using NServiceBus;
+
+partial class SendLocalOneOnOneRunner
+{
+    public IBus Bus { get; set; }
+
+    void SendLocal(Command msg)
+    {
+        Bus.SendLocal(msg);
+    }
+
+    public partial class Handler : IHandleMessages<Command>
+    {
+        public IBus Bus { get; set; }
+
+        public void Handle(Command message)
+        {
+            if(Shutdown) return;
+            Bus.SendLocal(message);
+            System.Threading.Interlocked.Increment(ref Count);
+        }
+    }
+}
+
+#endif
