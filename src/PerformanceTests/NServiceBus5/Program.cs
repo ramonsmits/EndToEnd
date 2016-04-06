c58062c09a6ae44ce65411422f87e0558142224b
Ramon Smits <ramon.smits@gmail.com>
2016-04-06 20:24:54 +0200

    Logging exceptions on host.

--- src/PerformanceTests/NServiceBus5/Program.cs
+++ src/PerformanceTests/NServiceBus5/Program.cs
@@ -12,23 +12,31 @@ namespace NServiceBus5
         static string endpointName = "PerformanceTests_" + AppDomain.CurrentDomain.FriendlyName.Replace(' ', '_');
         static void Main(string[] args)
         {
-            Log.Env();
-
-            var permutation = PermutationParser.FromCommandlineArgs();
-            var options = BusCreationOptions.Parse(args);
-
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            var tasks = permutation.Tests.Select(x => (IStartAndStop)assembly.CreateInstance(x)).ToArray();
-
-            var runDuration = TimeSpan.FromMinutes(1);
-
-            using (var bus = CreateBus(options, permutation))
+            try
             {
-                TestRunner.EndpointName = endpointName;
-                TestRunner.RunTests(bus, options);
-                foreach (var t in tasks) t.Start();
-                System.Threading.Thread.Sleep(runDuration);
-                foreach (var t in tasks) t.Stop();
+                Log.Env();
+
+                var permutation = PermutationParser.FromCommandlineArgs();
+                var options = BusCreationOptions.Parse(args);
+
+                var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                var tasks = permutation.Tests.Select(x => (IStartAndStop)assembly.CreateInstance(x)).ToArray();
+
+                var runDuration = TimeSpan.FromMinutes(1);
+
+                using (var bus = CreateBus(options, permutation))
+                {
+                    TestRunner.EndpointName = endpointName;
+                    TestRunner.RunTests(bus, options);
+                    foreach (var t in tasks) t.Start();
+                    System.Threading.Thread.Sleep(runDuration);
+                    foreach (var t in tasks) t.Stop();
+                }
+            }
+            catch (Exception ex)
+            {
+                NServiceBus.Logging.LogManager.GetLogger(typeof(Program)).Fatal("Main", ex);
+                throw;
             }
         }
 
