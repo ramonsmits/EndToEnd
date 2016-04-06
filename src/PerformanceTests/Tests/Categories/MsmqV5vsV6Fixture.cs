163f53f035652653e2d132ff0ea8e8b3bd5cb86d
Ramon Smits <ramon.smits@gmail.com>
2016-04-06 20:18:32 +0200

    Added fixture with only 4 permutations.

---
+++ src/PerformanceTests/Tests/Categories/MsmqV5vsV6Fixture.cs
@@ -0,0 +1,37 @@
+namespace Categories
+{
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using Tests.Permutations;
+    using Variables;
+
+    [TestFixture(Description = "Audit forwarding On / Off", Category = "Performance"), Explicit]
+    public class MsmqV5vsV6Fixture : Base
+    {
+        [TestCaseSource(nameof(CreatePermutations))]
+        public override void GatedSendLocalRunner(Permutation permutation)
+        {
+            base.GatedSendLocalRunner(permutation);
+        }
+
+        static IEnumerable<Permutation> CreatePermutations()
+        {
+            return PermutationGenerator.Generate(new Permutations
+            {
+                Versions = new[] { NServiceBusVersion.V5, NServiceBusVersion.V6, },
+                IOPS = new[] { IOPS.Default },
+                Platforms = new[] { Platform.x86, },
+                GarbageCollectors = new[] { GarbageCollector.Client, },
+                Transports = new[] { Transport.MSMQ },
+                Persisters = new[] { Persistence.InMemory },
+                Serializers = new[] { Serialization.Json, },
+                MessageSizes = new[] { MessageSize.Tiny, },
+                OutboxModes = new[] { Outbox.Off, },
+                DTCModes = new[] { DTC.On, },
+                TransactionMode = new[] { TransactionMode.Default, },
+                AuditModes = new[] { Audit.Off, Audit.On, },
+                ConcurrencyLevels = new[] { ConcurrencyLevel.EnvCores }
+            });
+        }
+    }
+}
