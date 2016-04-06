82681c28be88d4e0e95fb1efb0af8bf096efd477
Igal Tabachnik <hmemcpy@gmail.com>
2016-04-06 12:54:04 +0300

    Adding more Tests

--- src/PerformanceTests/Tests/Integration/PermutationDirectoryResolverTests.cs
+++ src/PerformanceTests/Tests/Integration/PermutationDirectoryResolverTests.cs
@@ -14,7 +14,7 @@
         [SetUp]
         public void Setup()
         {
-            resolver = new PermutationDirectoryResolver(TestContext.CurrentContext.WorkDirectory);
+            resolver = new PermutationDirectoryResolver(TestContext.CurrentContext.TestDirectory);
         }
 
         [Test]
@@ -32,19 +32,44 @@
             Assert.That(result.Files, Is.Not.Empty);
         }
 
-        [Test]
-        public void Should_resolve_persisters(Persistence persister, NServiceBusVersion version, string fileName)
+        [TestCase(NServiceBusVersion.V5, Persistence.InMemory, "Persistence.V5.InMemory.dll")]
+        [TestCase(NServiceBusVersion.V5, Persistence.NHibernate, "Persistence.V5.NHibernate_v6.dll")]
+        [TestCase(NServiceBusVersion.V5, Persistence.RavenDB, "Persistence.V5.RavenDB_v3.dll")]
+        [TestCase(NServiceBusVersion.V6, Persistence.InMemory, "Persistence.V6.InMemory.dll")]
+        [TestCase(NServiceBusVersion.V6, Persistence.NHibernate, "Persistence.V6.NHibernate_v7.dll")]
+        [TestCase(NServiceBusVersion.V6, Persistence.RavenDB, "Persistence.V6.RavenDB_v4.dll")]
+        public void Should_resolve_persisters(NServiceBusVersion version, Persistence persister, string fileName)
         {
             var permutation = new Permutation
             {
                 Persister = persister,
-                Transport = Transport.MSMQ,
                 Version = version
             };
 
             var result = resolver.Resolve(permutation);
-            Assert.That(result.Files.Any(info => info.Name == "Transport.V6.RabbitMQ_v4.dll"));
-            Assert.That(result.Files.Any(info => info.Name == "Persistence.V6.NHibernate_v7.dll"));
+            Assert.That(() => result.Files.Any(info => info.Name == fileName), "The name {0} was not found in {1}", fileName, string.Join(", ", result.Files.Select(f => f.Name)));
+        }
+
+        [TestCase(NServiceBusVersion.V5, Transport.MSMQ, "Transport.V5.MSMQ.dll")]
+        [TestCase(NServiceBusVersion.V5, Transport.AzureServiceBus, "Transport.V5.AzureServiceBus_v6.dll")]
+        [TestCase(NServiceBusVersion.V5, Transport.AzureStorageQueues, "Transport.V5.AzureStorageQueues_v6.dll")]
+        [TestCase(NServiceBusVersion.V5, Transport.RabbitMQ, "Transport.V5.RabbitMQ_v3.dll")]
+        [TestCase(NServiceBusVersion.V5, Transport.SQLServer, "Transport.V5.SQLServer_v2.dll")]
+        [TestCase(NServiceBusVersion.V6, Transport.MSMQ, "Transport.V6.MSMQ.dll")]
+        [TestCase(NServiceBusVersion.V6, Transport.AzureServiceBus, "Transport.V6.AzureServiceBus_v7.dll")]
+        [TestCase(NServiceBusVersion.V6, Transport.AzureStorageQueues, "Transport.V6.AzureStorageQueues_v7.dll")]
+        [TestCase(NServiceBusVersion.V6, Transport.RabbitMQ, "Transport.V6.RabbitMQ_v4.dll")]
+        [TestCase(NServiceBusVersion.V6, Transport.SQLServer, "Transport.V6.SQLServer_v3.dll")]
+        public void Should_resolve_transports(NServiceBusVersion version, Transport transport, string fileName)
+        {
+            var permutation = new Permutation
+            {
+                Transport= transport,
+                Version = version
+            };
+
+            var result = resolver.Resolve(permutation);
+            Assert.That(() => result.Files.Any(info => info.Name == fileName), "The name {0} was not found in {1}", fileName, string.Join(", ", result.Files.Select(f => f.Name)));
         }
 
         [Test]
