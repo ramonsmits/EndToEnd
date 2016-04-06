115435c6fc9c1588bfa4b8990adf95b2ab31f8ff
Ramon Smits <ramon.smits@gmail.com>
2016-04-06 22:06:42 +0200

    Removed ICreateTestData implementation, not working. The bus probably needs to be started first for the values to be available.

--- src/PerformanceTests/Transport.V5.MSMQ.V5/MsmqProfile.cs
+++ src/PerformanceTests/Transport.V5.MSMQ.V5/MsmqProfile.cs
@@ -5,31 +5,11 @@ using NServiceBus;
 using NServiceBus.Configuration.AdvanceExtensibility;
 using NServiceBus.Unicast.Queuing;
 
-class MsmqProfile : IProfile, ICreateTestData
+class MsmqProfile : IProfile
 {
-    string endpointName = @".\private$\performancetests_nservicebus5.vshost.exe";
-
     public void Configure(BusConfiguration busConfiguration)
     {
         busConfiguration.UseTransport<MsmqTransport>()
             .ConnectionString("deadLetter=false;journal=false");
-    }
-
-    public void CreateTestData(BusConfiguration configuration)
-    {
-        configuration.GetSettings().Get<IWantQueueCreated>();
-        
-        configuration.GetSettings().EndpointName();
-    }
-
-    public void CleanUpTestData(BusConfiguration configuration)
-    {
-        
-
-        if (!MessageQueue.Exists(endpointName))
-            return;
-
-        var queue = new MessageQueue(endpointName);
-        queue.Purge();
     }
 }
