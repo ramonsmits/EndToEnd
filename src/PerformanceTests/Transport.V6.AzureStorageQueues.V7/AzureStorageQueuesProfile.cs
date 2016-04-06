3f6f964ce99850853d68e9c87b22586db1d05aca
Ramon Smits <ramon.smits@gmail.com>
2016-04-07 01:09:31 +0200

    Azure Storage Queue connection strings via config.

--- src/PerformanceTests/Transport.V6.AzureStorageQueues.V7/AzureStorageQueuesProfile.cs
+++ src/PerformanceTests/Transport.V6.AzureStorageQueues.V7/AzureStorageQueuesProfile.cs
@@ -9,36 +9,38 @@ using NServiceBus;
 using NServiceBus.Azure.Transports.WindowsAzureStorageQueues.Config;
 using NServiceBus.Configuration.AdvanceExtensibility;
 
-class AzureStorageQueuesProfile : IProfile, ICreateTestData
+class AzureStorageQueuesProfile : IProfile
 {
     public void Configure(EndpointConfiguration endpointConfiguration)
     {
-        endpointConfiguration.UseTransport<AzureStorageQueueTransport>();
+        endpointConfiguration
+            .UseTransport<AzureStorageQueueTransport>()
+            .ConnectionStringName("AzureStorageQueue");
     }
 
-    public void CreateTestData(EndpointConfiguration configuration)
-    {
-        throw new NotImplementedException();
-    }
+    //public void CreateTestData(EndpointConfiguration configuration)
+    //{
+    //    throw new NotImplementedException();
+    //}
 
-    public void CleanUpTestData(EndpointConfiguration configuration)
-    {
-        var connectionString = configuration.GetSettings().GetOrDefault<string>(WellKnownConfigurationKeys.ReceiverConnectionString);
+    //public void CleanUpTestData(EndpointConfiguration configuration)
+    //{
+    //    var connectionString = configuration.GetSettings().GetOrDefault<string>(WellKnownConfigurationKeys.ReceiverConnectionString);
 
-        var storageAccount = CloudStorageAccount.Parse(connectionString);
-        var queueClient = storageAccount.CreateCloudQueueClient();
-        IEnumerable<CloudQueue> allQueues = queueClient.ListQueues().ToArray();
+    //    var storageAccount = CloudStorageAccount.Parse(connectionString);
+    //    var queueClient = storageAccount.CreateCloudQueueClient();
+    //    IEnumerable<CloudQueue> allQueues = queueClient.ListQueues().ToArray();
 
-        var countdown = new CountdownEvent(allQueues.Count());
+    //    var countdown = new CountdownEvent(allQueues.Count());
 
-        foreach (var queue in allQueues)
-        {
-            queue.ClearAsync().ContinueWith(t => countdown.Signal());
-        }
+    //    foreach (var queue in allQueues)
+    //    {
+    //        queue.ClearAsync().ContinueWith(t => countdown.Signal());
+    //    }
 
-        if (countdown.Wait(TimeSpan.FromMinutes(1)) == false)
-        {
-            throw new TimeoutException("Waiting for cleaning queues took too much.");
-        }
-    }
+    //    if (countdown.Wait(TimeSpan.FromMinutes(1)) == false)
+    //    {
+    //        throw new TimeoutException("Waiting for cleaning queues took too much.");
+    //    }
+    //}
 }
