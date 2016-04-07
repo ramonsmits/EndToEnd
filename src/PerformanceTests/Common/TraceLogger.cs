da24ccca7424c0aa84a809b7db0e71c95b50a6f9
Hadi Eskandari <h.eskandari@gmail.com>
2016-04-07 18:34:29 +1000

    Plugging in splunk trace listener for test run logs

---
+++ src/PerformanceTests/Common/TraceLogger.cs
@@ -0,0 +1,26 @@
+﻿namespace Utils
+{
+    using System;
+    using System.Configuration;
+    using System.Diagnostics;
+    using Splunk.Logging;
+
+    public class TraceLogger
+    {
+        public static void Initialize()
+        {
+            var url = ConfigurationManager.AppSettings["SplunkURL"];
+            var port = int.Parse(ConfigurationManager.AppSettings["SplunkPort"]);
+
+            var listener = new TcpTraceListener(url, port, new ExponentialBackoffTcpReconnectionPolicy());
+            Trace.Listeners.Add(listener);
+
+            listener.AddLoggingFailureHandler(ex => 
+            {
+                Console.Error.WriteLine("Splunk logger failed: {0}", ex);
+            });
+
+            Trace.WriteLine($"Splunk Tracelogger configured at {url}:{port}");
+        }
+    }
+}
