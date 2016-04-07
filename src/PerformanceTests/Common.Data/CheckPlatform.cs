a1f1f9d3e6ed7ec139b4c82647f1be5848e16b69
Ramon Smits <ramon.smits@gmail.com>
2016-04-07 12:27:00 +0200

    Post build step to generate 32 exe using corflags.exe and check at startup to validate correct environment.

---
+++ src/PerformanceTests/Common.Data/CheckPlatform.cs
@@ -0,0 +1,20 @@
+﻿using System;
+#if Version6
+using Configuration = NServiceBus.EndpointConfiguration;
+#else
+using Configuration = NServiceBus.BusConfiguration;
+#endif
+using NServiceBus;
+using Tests.Permutations;
+using Variables;
+
+class CheckPlatform : IProfile, INeedPermutation
+{
+    public Permutation Permutation { private get; set; }
+
+    public void Configure(Configuration cfg)
+    {
+        if (Permutation.Platform == Platform.x64 && !Environment.Is64BitProcess) throw new InvalidOperationException("Environment is x86 but must be x64.");
+        if (Permutation.Platform == Platform.x86 && Environment.Is64BitProcess) throw new InvalidOperationException("Environment is x64 but must be x86.");
+    }
+}
