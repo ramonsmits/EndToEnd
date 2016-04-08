399f5d774f27a351aa5ce5a4c979417317cd32ec
Ramon Smits <ramon.smits@gmail.com>
2016-04-08 16:28:33 +0200

    Log loaded assemblies.

--- src/PerformanceTests/Common.Data/AssemblyScanner.cs
+++ src/PerformanceTests/Common.Data/AssemblyScanner.cs
@@ -1,17 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using NServiceBus.Logging;
 
 public static class AssemblyScanner
 {
     public static IEnumerable<Assembly> GetAssemblies()
     {
+        var l = LogManager.GetLogger(typeof(AssemblyScanner));
+
         foreach (var path in Directory.EnumerateFiles(Environment.CurrentDirectory, "*.dll"))
         {
             Assembly.LoadFile(path);
         }
 
-        return AppDomain.CurrentDomain.GetAssemblies();
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        foreach (var a in assemblies.OrderBy(a=>a.ToString()))
+        {
+            l.InfoFormat("Loaded: {0}", a);
+        }
+
+        return assemblies;
+
+
     }
 }
