d3718ea362233cf417d33894a2c5849e9f02cb15
Ramon Smits <ramon.smits@gmail.com>
2016-04-09 13:44:22 +0200

    Logging of environment restructured to identify settings more easily and added .net framework version.

--- src/PerformanceTests/Common.Data/EnvironmentStats.cs
+++ src/PerformanceTests/Common.Data/EnvironmentStats.cs
@@ -6,14 +6,20 @@ static class EnvironmentStats
 {
     public static void Write()
     {
-        var log = LogManager.GetLogger("Environment");
-        log.InfoFormat("IsServerGC:{0} ({1})", System.Runtime.GCSettings.IsServerGC, System.Runtime.GCSettings.LatencyMode);
+        var log = LogManager.GetLogger("GCSettings");
+        log.InfoFormat("IsServerGC: {0}", System.Runtime.GCSettings.IsServerGC);
+        log.InfoFormat("LatencyMode: {0}", System.Runtime.GCSettings.LatencyMode);
+
+        log = LogManager.GetLogger("Environment");
         log.InfoFormat("ProcessorCount: {0}", Environment.ProcessorCount);
         log.InfoFormat("64bit: {0}", Environment.Is64BitProcess);
-        log.InfoFormat("ServicePointManager.DefaultConnectionLimit: {0}", ServicePointManager.DefaultConnectionLimit);
-        log.InfoFormat("ServicePointManager.DefaultNonPersistentConnectionLimit: {0}", ServicePointManager.DefaultNonPersistentConnectionLimit);
-        log.InfoFormat("ServicePointManager.EnableDnsRoundRobin: {0}", ServicePointManager.EnableDnsRoundRobin);
-        log.InfoFormat("ServicePointManager.Expect100Continue: {0}", ServicePointManager.Expect100Continue);
-        log.InfoFormat("ServicePointManager.UseNagleAlgorithm: {0}", ServicePointManager.UseNagleAlgorithm);
+        log.InfoFormat("Version: {0}", Environment.Version);
+
+        log = LogManager.GetLogger("ServicePointManager");
+        log.InfoFormat("DefaultConnectionLimit: {0}", ServicePointManager.DefaultConnectionLimit);
+        log.InfoFormat("DefaultNonPersistentConnectionLimit: {0}", ServicePointManager.DefaultNonPersistentConnectionLimit);
+        log.InfoFormat("EnableDnsRoundRobin: {0}", ServicePointManager.EnableDnsRoundRobin);
+        log.InfoFormat("Expect100Continue: {0}", ServicePointManager.Expect100Continue);
+        log.InfoFormat("UseNagleAlgorithm: {0}", ServicePointManager.UseNagleAlgorithm);
     }
 }
