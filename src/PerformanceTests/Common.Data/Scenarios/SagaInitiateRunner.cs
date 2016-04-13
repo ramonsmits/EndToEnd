ca55634585b2df9f10262c31012ed614172d8e4a
Dennis van der Stelt <dvdstelt@outlook.com>
2016-04-13 11:21:48 +0200

    Moved to IThrow interface

---
+++ src/PerformanceTests/Common.Data/Scenarios/SagaInitiateRunner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using NServiceBus;
+using Tests.Permutations;
+using Variables;
+
+partial class SagaInitiateRunner : BaseRunner, IThrowIfPermutationIsNotAllowed
+{
+    public void ThrowIfPermutationIsNotAllowed(Permutation permutation)
+    {
+        if (permutation.Persister == Persistence.InMemory)
+            throw new InvalidOperationException("This test has no use running with InMemory persistence.");
+    }
+
+    protected override void Start()
+    {
+    }
+
+    protected override void Stop()
+    {
+    }
+
+    public class Command : ICommand
+    {
+        public Command(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+
+}
