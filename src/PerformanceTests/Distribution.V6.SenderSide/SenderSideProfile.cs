d455729302bb502bc169ff4837c1aaf9c3c89424
Ramon Smits <ramon.smits@gmail.com>
2016-04-14 18:26:30 +0200

    Added Distribution.V6.SenderSide

---
+++ src/PerformanceTests/Distribution.V6.SenderSide/SenderSideProfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using NServiceBus;
+using NServiceBus.Routing;
+using Variables;
+
+class SenderSideProfile : IProfile, INeedContext
+{
+    public IContext Context { private get; set; }
+
+    public void Configure(EndpointConfiguration cfg)
+    {
+        if (Context.Permutation.ScaleOut != ScaleOut.SenderSide) return;
+        if (Context.Permutation.Transport != Transport.MSMQ) throw new NotSupportedException("SenderSide should only be used with MSMQ");
+
+        var senderSideArgs = this.FetchSetting("SenderSide").Split(';');
+
+        cfg.SendFailedMessagesTo("error@" + senderSideArgs[0]);
+
+        var machines = senderSideArgs[1].Split('|');
+
+        var routing = cfg.UnicastRouting();
+
+        var endpoint = new EndpointName(Context.EndpointName);
+
+        var instances = machines.Select(x => new EndpointInstance(endpoint, x)).ToArray();
+        routing.Mapping.Physical.Add(endpoint, instances);
+    }
+}
