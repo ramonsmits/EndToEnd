aab0ccd802867191ea625862dce0df2f376785ee
Tim Bussmann <tim.bussmann@particular.net>
2016-04-14 15:10:08 +0200

    remove splunk packages

--- src/PerformanceTests/Utils/EnumExtensionMethods.cs
+++ src/PerformanceTests/Utils/EnumExtensionMethods.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tests
 {
@@ -12,16 +7,11 @@ namespace Tests
     {
         public static string GetEnumDescription(this Enum value)
         {
-            FieldInfo fi = value.GetType().GetField(value.ToString());
+            var fi = value.GetType().GetField(value.ToString());
 
             var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
 
-            if (attributes != null && attributes.Length > 0)
-            {
-                return attributes[0].Description;
-            }
-
-            return value.ToString();
+            return attributes.Length > 0 ? attributes[0].Description : value.ToString();
         }
     }
 }
