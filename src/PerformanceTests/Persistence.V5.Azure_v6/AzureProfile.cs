3a9a9cdf95508f66bdade1e7ddebd7da4c612bf5
Ramon Smits <ramon.smits@gmail.com>
2016-04-14 14:35:50 +0200

    NServiceBus V5 Azure storage connection strings.

--- src/PerformanceTests/Persistence.V5.Azure_v6/AzureProfile.cs
+++ src/PerformanceTests/Persistence.V5.Azure_v6/AzureProfile.cs
@@ -1,9 +1,22 @@
-﻿using NServiceBus;
+﻿using System;
+using NServiceBus;
+using NServiceBus.Persistence;
+using NServiceBus.SagaPersisters;
+using Tests.Permutations;
+using Variables;
 
-public class AzureProfile : IProfile
+public class AzureProfile : IProfile, INeedPermutation
 {
-    public void Configure(BusConfiguration busConfiguration)
+    public Permutation Permutation { private get; set; }
+
+    public void Configure(BusConfiguration cfg)
     {
-        busConfiguration.UsePersistence<AzureStoragePersistence>();
+        if (Permutation.OutboxMode == Outbox.On) throw new NotSupportedException("Outbox is not supported with Azure storage.");
+
+        var connectionString = this.GetConnectionString("AzureStorage");
+        cfg.UsePersistence<AzureStoragePersistence, StorageType.Sagas>().ConnectionString(connectionString);
+        cfg.UsePersistence<AzureStoragePersistence, StorageType.Subscriptions>().ConnectionString(connectionString);
+        cfg.UsePersistence<AzureStoragePersistence, StorageType.Timeouts>().ConnectionString(connectionString);
     }
+
 }
