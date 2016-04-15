30a798b0391068970bb88c8c4c570e627a03af69
Dennis van der Stelt <dvdstelt@outlook.com>
2016-04-15 14:09:59 +0200

    Added SendRunner for testing Send only

---
+++ src/PerformanceTests/Common/Scenarios/SendRunner.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Threading.Tasks;
+#if Version6
+using Configuration = NServiceBus.EndpointConfiguration;
+#else
+using Configuration = NServiceBus.BusConfiguration;
+#endif
+using NServiceBus;
+
+/// <summary>
+/// Does a continuous test where a pre-seeded amount of messages will be handled
+/// </summary>    
+class SendRunner : LoopRunner
+{
+    protected override void Start()
+    {
+    }
+
+    protected override void Stop()
+    {
+    }
+
+    protected override Task Loop(object o)
+    {
+        while (!Shutdown)
+        {
+            try
+            {
+
+            }
+    }
+
+    public class Command : ICommand
+    {
+    }
+}
+
