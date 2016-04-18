95a3cd64a828396d238987f804793f8328229f57
Ramon Smits <ramon.smits@gmail.com>
2016-04-18 19:03:27 +0200

    Message seeding parallelism extracted to helper class to swap implementation.

---
+++ src/PerformanceTests/Utils/TaskHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class TaskHelper
+{
+    public static async Task ParallelFor(int count, Func<Task> action)
+    {
+        var sends = new Task[count];
+        for (var i = 0; i < count; i++) sends[i] = action();
+        await Task.WhenAll(sends);
+    }
+}
