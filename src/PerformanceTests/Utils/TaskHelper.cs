b3de3eea06222d086db77707af4da00d6d4ab89a
Ramon Smits <ramon.smits@gmail.com>
2016-04-18 19:17:21 +0200

    Implementation based on @danielmarbach

--- src/PerformanceTests/Utils/TaskHelper.cs
+++ src/PerformanceTests/Utils/TaskHelper.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 public class TaskHelper
 {
+    static readonly int cores = Environment.ProcessorCount;
+
     public static async Task ParallelFor(int count, Func<Task> action)
     {
-        var sends = new Task[count];
-        for (var i = 0; i < count; i++) sends[i] = action();
-        await Task.WhenAll(sends);
+        // If we have a batch of 10,000 and have 10 cores we do 10 iterations of 1,000 message chunks.
+        for (var i = 0; i < cores; i++)
+        {
+            var chunkSize = count / cores;
+            var sends = new List<Task>(chunkSize);
+            for (var j = 0; j < chunkSize; j++)
+            {
+                sends.Add(action());
+            }
+            await Task.WhenAll(sends);
+        }
     }
+
 }
+
