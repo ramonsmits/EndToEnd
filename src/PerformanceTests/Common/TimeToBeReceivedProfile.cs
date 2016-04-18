507b8e62ee2c9a20492a7bb625b6c15b098225c5
Ramon Smits <ramon.smits@gmail.com>
2016-04-18 12:57:40 +0200

    Added TimeToBeReceivedProfile

---
+++ src/PerformanceTests/Common/TimeToBeReceivedProfile.cs
@@ -0,0 +1,17 @@
+﻿#if Version6
+using Cfg = NServiceBus.EndpointConfiguration;
+using NServiceBus;
+#else
+using Cfg = NServiceBus.BusConfiguration;
+#endif
+
+using System;
+
+class TimeToBeReceivedProfile : IProfile
+{
+    static readonly TimeSpan TTBR = TimeSpan.FromMinutes(1);
+    public void Configure(Cfg cfg)
+    {
+        cfg.Conventions().DefiningTimeToBeReceivedAs(type => TTBR);
+    }
+}
