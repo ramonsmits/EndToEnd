0e01e1974f8973c0143ff5d1ccb5af2fcf92ad15
Ramon Smits <ramon.smits@gmail.com>
2016-04-19 16:50:00 +0200

    ASQ profiles check if the message size is supported.

--- src/PerformanceTests/Transport.V5.AzureStorageQueues.V6/AzureStorageQueueProfile.cs
+++ src/PerformanceTests/Transport.V5.AzureStorageQueues.V6/AzureStorageQueueProfile.cs
@@ -1,9 +1,15 @@
-﻿using NServiceBus;
+﻿using System;
+using NServiceBus;
+using Variables;
 
-class AzureStorageQueueProfile : IProfile
+class AzureStorageQueueProfile : IProfile, INeedContext
 {
+    public IContext Context { private get; set; }
+
     public void Configure(BusConfiguration busConfiguration)
     {
+        if ((int)MessageSize.Medium < (int)Context.Permutation.MessageSize) throw new NotSupportedException($"Message size {Context.Permutation.MessageSize} not supported by ASQ.");
+
         busConfiguration
             .UseTransport<AzureStorageQueueTransport>()
             .ConnectionString(this.GetConnectionString("AzureStorageQueue"));
--- src/PerformanceTests/Transport.V6.AzureStorageQueues.V7/AzureStorageQueuesProfile.cs
+++ src/PerformanceTests/Transport.V6.AzureStorageQueues.V7/AzureStorageQueuesProfile.cs
@@ -1,11 +1,18 @@
-﻿using NServiceBus;
+﻿using System;
+using NServiceBus;
+using Variables;
 
-class AzureStorageQueuesProfile : IProfile
+class AzureStorageQueuesProfile : IProfile, INeedContext
 {
+    public IContext Context { private get; set; }
+
     public void Configure(EndpointConfiguration endpointConfiguration)
     {
+        if ((int)MessageSize.Medium < (int)Context.Permutation.MessageSize) throw new NotSupportedException($"Message size {Context.Permutation.MessageSize} not supported by ASQ.");
+
         endpointConfiguration
             .UseTransport<AzureStorageQueueTransport>()
             .ConnectionString(this.GetConnectionString("AzureStorageQueue"));
     }
+
 }
