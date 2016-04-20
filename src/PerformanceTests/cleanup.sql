3d96736f6b3371672794c882e5b63e24b5c825c8
Ramon Smits <ramon.smits@gmail.com>
2016-04-20 17:25:21 +0200

    NHibernate persistence and SQL transport now use their own V5/V6 namespace. Included cleanup.sql script to create schemas and truncate all tables.

---
+++ src/PerformanceTests/cleanup.sql
@@ -0,0 +1,11 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'V5')
+BEGIN
+	EXEC('CREATE SCHEMA V5')
+END
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'V6')
+BEGIN
+	EXEC('CREATE SCHEMA V6')
+END
+
+EXEC sp_msforeachtable @command1='TRUNCATE TABLE ?',@whereand='AND Schema_Id IN (Schema_id(''V5''),Schema_id(''V6''))'
