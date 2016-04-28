686197a3753eb7c7cd8939808234662c05e04d55
Ramon Smits <ramon.smits@gmail.com>
2016-04-28 12:51:08 +0200

    wip

---
+++ src/PerformanceTests/Persistence.V5.PostgreSQL/PostgreSqlProfile.cs
@@ -0,0 +1,10 @@
+﻿using NServiceBus;
+using NServiceBus.PostgreSQL;
+
+class RavenDBProfile : IProfile
+{
+    public void Configure(BusConfiguration cfg)
+    {
+        cfg.UsePersistence<PostgreSQLPersistence>();
+    }
+}
