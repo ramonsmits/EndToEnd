e4c674a346051395d7431ee4f749817d0c46fe57
Ramon Smits <ramon.smits@gmail.com>
2016-04-28 13:05:52 +0200

    Init script was empty for V5 persistence.

--- src/PerformanceTests/Persistence.V5.NHibernate.V6/init.sql
+++ src/PerformanceTests/Persistence.V5.NHibernate.V6/init.sql
@@ -1,1 +1,7 @@
-﻿
+﻿IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'V5')
+BEGIN
+	EXEC('CREATE SCHEMA V5')
+END
+
+-- truncate all tables
+EXEC sp_msforeachtable @command1='TRUNCATE TABLE ?',@whereand='AND Schema_Id = Schema_id(''V5'')'
