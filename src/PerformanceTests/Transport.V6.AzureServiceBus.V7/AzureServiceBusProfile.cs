4baca678eb4f9cb70bf7af321b9c1b9ad542e718
Ramon Smits <ramon.smits@gmail.com>
2016-04-28 11:30:16 +0200

    Removed topolog cleanup logic as this should not be executed from within a test run.

--- src/PerformanceTests/Transport.V6.AzureServiceBus.V7/AzureServiceBusProfile.cs
+++ src/PerformanceTests/Transport.V6.AzureServiceBus.V7/AzureServiceBusProfile.cs
@@ -1,16 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.ServiceBus;
-using NServiceBus;
+﻿using NServiceBus;
 using NServiceBus.AzureServiceBus;
-using NServiceBus.Logging;
 
-class AzureServiceBusProfile : IProfile, ISetup
+class AzureServiceBusProfile : IProfile
 {
     readonly string connectionstring = ConfigurationHelper.GetConnectionString("AzureServiceBus");
-    ILog Log = LogManager.GetLogger(nameof(AzureServiceBusProfile));
 
     public void Configure(EndpointConfiguration endpointConfiguration)
     {
@@ -18,52 +11,5 @@ class AzureServiceBusProfile : IProfile, ISetup
             .UseTransport<AzureServiceBusTransport>()
             .UseTopology<ForwardingTopology>()
             .ConnectionString(connectionstring);
-    }
-
-    void ISetup.Setup()
-    {
-        Log.WarnFormat("Skipping topology cleanup!");
-        //CleanupTopology();
-    }
-
-    void CleanupTopology()
-    {
-        var manager = NamespaceManager.CreateFromConnectionString(connectionstring);
-
-        var tasks = new List<Task>();
-        tasks.AddRange(DeleteQueues(manager));
-        tasks.AddRange(DeleteTopics(manager));
-
-        Log.Info("Waiting for delete tasks to finish...");
-        Task.WhenAll(tasks);
-
-        var topicCount = 0;
-        var queueCount = 0;
-
-        while (0 < (topicCount = manager.GetTopics().Count()) || 0 < (queueCount = manager.GetQueues().Count()))
-        {
-            Thread.Sleep(100);
-            Log.InfoFormat("Waiting until all topics ({0}) and queues ({1}) are deleted...", topicCount, queueCount);
-        }
-
-        Log.Info("Cleanup completed!");
-    }
-
-    IEnumerable<Task> DeleteQueues(NamespaceManager manager)
-    {
-        foreach (var q in manager.GetQueues())
-        {
-            Log.InfoFormat("Deleting queue: {0}", q.Path);
-            yield return manager.DeleteQueueAsync(q.Path);
-        }
-    }
-
-    IEnumerable<Task> DeleteTopics(NamespaceManager manager)
-    {
-        foreach (var t in manager.GetTopics())
-        {
-            Log.InfoFormat("Deleting topic: {0}", t.Path);
-            yield return manager.DeleteQueueAsync(t.Path);
-        }
     }
 }
