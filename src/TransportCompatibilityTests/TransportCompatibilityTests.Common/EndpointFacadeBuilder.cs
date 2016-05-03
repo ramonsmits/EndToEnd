33b955e1c75ab1fb2fe19f932860f95d2c634108
John Simons <john.simons@nservicebus.com>
2016-05-03 13:41:54 +1000

    Unload AppDomain
    Otherwise memory leak

--- src/TransportCompatibilityTests/TransportCompatibilityTests.Common/EndpointFacadeBuilder.cs
+++ src/TransportCompatibilityTests/TransportCompatibilityTests.Common/EndpointFacadeBuilder.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using TransportCompatibilityTests.Common.Messages;
 
     public class EndpointFacadeBuilder
     {
@@ -24,9 +25,63 @@
 
             var facade = (IEndpointFacade)appDomain.CreateInstanceFromAndUnwrap(assemblyPath, typeName);
             facade.Bootstrap(endpointDefinition);
+            
+            return new MyWrapper(facade, appDomain);
+        }
+    }
 
-            return facade;
+    class MyWrapper : IEndpointFacade
+    {
+        private readonly IEndpointFacade facade;
+        private readonly AppDomain domain;
+
+        public MyWrapper(IEndpointFacade facade, AppDomain domain)
+        {
+            this.facade = facade;
+            this.domain = domain;
         }
 
+        public void Dispose()
+        {
+            facade.Dispose();
+            AppDomain.Unload(domain);
+        }
+
+        public void Bootstrap(EndpointDefinition endpointDefinition)
+        {
+            facade.Bootstrap(endpointDefinition);
+        }
+
+        public void SendCommand(Guid messageId)
+        {
+            facade.SendCommand(messageId);
+        }
+
+        public void SendRequest(Guid requestId)
+        {
+            facade.SendRequest(requestId);
+        }
+
+        public void PublishEvent(Guid eventId)
+        {
+            facade.PublishEvent(eventId);
+        }
+
+        public void SendAndCallbackForInt(int value)
+        {
+            facade.SendAndCallbackForInt(value);
+        }
+
+        public void SendAndCallbackForEnum(CallbackEnum value)
+        {
+            facade.SendAndCallbackForEnum(value);
+        }
+
+        public Guid[] ReceivedMessageIds => facade.ReceivedMessageIds;
+        public Guid[] ReceivedResponseIds => facade.ReceivedResponseIds;
+        public Guid[] ReceivedEventIds => facade.ReceivedEventIds;
+        public int[] ReceivedIntCallbacks => facade.ReceivedIntCallbacks;
+        public CallbackEnum[] ReceivedEnumCallbacks => facade.ReceivedEnumCallbacks;
+        public int NumberOfSubscriptions => facade.NumberOfSubscriptions;
     }
 }
