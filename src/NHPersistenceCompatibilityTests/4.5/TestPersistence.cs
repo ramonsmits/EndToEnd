60654b8c6f5049082d8f5ae0c5428ee8b296f55b
Tomasz Masternak <tomasz.masternak@particular.net>
2016-05-17 10:06:31 +0200

    basic 4.5 test project

---
+++ src/NHPersistenceCompatibilityTests/4.5/TestPersistence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PersistenceCompatibilityTests;
+
+namespace _4._5
+{
+    public class TestPersistence : MarshalByRefObject, ITestPersistence
+    {
+        public void Persist(Guid id, string originator)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify(Guid id, string originator)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
