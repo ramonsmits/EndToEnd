fedc8e76104b12439049101abf1098dbe1549ce5
Ramon Smits <ramon.smits@gmail.com>
2016-05-30 18:29:51 +0200

    Added AzureFixture to check Azure transport in combination with Azure persistence.

---
+++ src/PerformanceTests/Tests/Categories/AzureFixture.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Tests.Categories
+{
+    using global::Categories;
+    using NUnit.Framework;
+    using Permutations;
+    using Variables;
+
+    [TestFixture(Description = "Persisters", Category = "Performance"), Explicit]
+    public class AzureFixture : Base
+    {
+        [TestCaseSource(nameof(CreatePermutations))]
+        public override void GatedPublishRunner(Permutation permutation)
+        {
+            base.GatedPublishRunner(permutation);
+        }
+
+        [TestCaseSource(nameof(CreatePermutations))]
+        public override void SagaInitiateRunner(Permutation permutation)
+        {
+            base.SagaInitiateRunner(permutation);
+        }
+
+        static IEnumerable<Permutation> CreatePermutations()
+        {
+            return PermutationGenerator.Generate(new Permutations
+            {
+                Transports = new[] { Transport.AzureStorageQueues, Transport.AzureServiceBus,  },
+                Persisters = new [] { Persistence.Azure, },
+                Serializers = new[] { Serialization.Json, },
+                OutboxModes = new[] { Outbox.Off, },
+            });
+        }
+    }
+}
