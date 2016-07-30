75052569005822569be31d848a33942de440e34b
Ramon Smits <ramon.smits@gmail.com>
2016-07-30 20:57:39 +0200

    Removed 'PrefetchSize' argument from connection string if present to that the connection string can be shared between V5 and V6.

--- src/PerformanceTests/Transport.V6.RabbitMQ.V4/RabbitMQProfile.cs
+++ src/PerformanceTests/Transport.V6.RabbitMQ.V4/RabbitMQProfile.cs
@@ -1,11 +1,16 @@
-﻿using NServiceBus;
+﻿using System.Data.Common;
+using NServiceBus;
 
 class RabbitMQProfile : IProfile
 {
     public void Configure(EndpointConfiguration endpointConfiguration)
     {
+        var cs = ConfigurationHelper.GetConnectionString("RabbitMQ");
+        var builder = new DbConnectionStringBuilder { ConnectionString = cs };
+        if (builder.Remove("prefetchcount")) NServiceBus.Logging.LogManager.GetLogger(nameof(RabbitMQProfile)).Warn("Removed 'prefetchcount' value from connection string.");
+
         endpointConfiguration
             .UseTransport<RabbitMQTransport>()
-            .ConnectionString(ConfigurationHelper.GetConnectionString("RabbitMQ"));
+            .ConnectionString(builder.ToString());
     }
 }
