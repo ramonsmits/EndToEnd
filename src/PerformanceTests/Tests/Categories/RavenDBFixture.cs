85b43820de533a9cc745ae94501b21e885b4827a
Ramon Smits <ramon.smits@gmail.com>
2016-08-30 13:11:10 +0200

    Added RavenDB fixture.

---
+++ src/PerformanceTests/Tests/Categories/RavenDBFixture.cs
@@ -0,0 +1,35 @@
+namespace Categories
+{
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using Tests.Permutations;
+    using Variables;
+
+    [TestFixture(Description = "RavenDB", Category = "Performance"), Explicit]
+    public class RavenDBConcurrencyForV6Fixture : Base
+    {
+        [TestCaseSource(nameof(CreatePermutations))]
+        public override void SagaInitiateRunner(Permutation permutation)
+        {
+            base.SagaInitiateRunner(permutation);
+        }
+
+        static IEnumerable<Permutation> CreatePermutations()
+        {
+            return PermutationGenerator.Generate(new Permutations
+            {
+                Versions = new[] { NServiceBusVersion.V6 },
+                Transports = new[] { Transport.MSMQ },
+                Persisters = new[] { Persistence.RavenDB },
+                Serializers = new[] { Serialization.Json },
+                OutboxModes = new[] { Outbox.Off },
+                TransactionMode = new [] {TransactionMode.Transactional, TransactionMode.Receive, },
+                ConcurrencyLevels = new[] {
+                    ConcurrencyLevel.Sequential,
+                    ConcurrencyLevel.EnvCores,
+                    ConcurrencyLevel.EnvCores4x,
+                }
+            });
+        }
+    }
+}
