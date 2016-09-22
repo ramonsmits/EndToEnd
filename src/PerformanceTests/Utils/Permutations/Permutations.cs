43a9eee60c5fc23cc467405f284d734f86372ee2
Ramon Smits <ramon.smits@gmail.com>
2016-09-22 11:00:02 +0200

    Max concurrency level to: logical cores * 64 to improve throughput on high latency transports.

--- src/PerformanceTests/Utils/Permutations/Permutations.cs
+++ src/PerformanceTests/Utils/Permutations/Permutations.cs
@@ -15,7 +15,7 @@ namespace Tests.Permutations
         public Transport[] Transports = (Transport[])Enum.GetValues(typeof(Transport));
         public GarbageCollector[] GarbageCollectors = { GarbageCollector.Server };
         public TransactionMode[] TransactionMode = { Variables.TransactionMode.Transactional };
-        public ConcurrencyLevel[] ConcurrencyLevels = { ConcurrencyLevel.EnvCores4x, };
+        public ConcurrencyLevel[] ConcurrencyLevels = { ConcurrencyLevel.EnvCores64x };
         public ScaleOut[] ScaleOuts = { ScaleOut.NoScaleOut };
     }
 }
